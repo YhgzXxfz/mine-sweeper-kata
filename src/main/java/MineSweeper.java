3dcbab7fc34df58895dec30636d2aed2db63b773
Qingfei.You <qingfeiyou@gmail.com>
2017-04-23 17:46:46 +0200

    feat(digit): update board when revealing empty square with adjacent mine

--- src/main/java/MineSweeper.java
+++ src/main/java/MineSweeper.java
@@ -6,6 +6,19 @@ public class MineSweeper {
     }
 
     public void update(int x, int y) {
-        board[x][y] = 'X';
+        if (board[x][y] == 'M') {
+            board[x][y] = 'X';
+            return ;
+        }
+
+        int rows = board.length, cols = board[0].length, numberOfMines = 0;
+        for (int i = Math.max(x-1, 0); i <= Math.min(x+1, rows-1); ++i) {
+            for (int j = Math.max(y-1, 0); j <= Math.min(y+1, cols-1); ++j) {
+                if (board[i][j] == 'M') {
+                    numberOfMines++;
+                }
+            }
+        }
+        board[x][y] = (char) ('0'+numberOfMines);
     }
 }
--- src/test/java/MineSweeperTest.java
+++ src/test/java/MineSweeperTest.java
@@ -11,10 +11,36 @@ public class MineSweeperTest {
         MineSweeper mineSweeper = new MineSweeper(board);
 
         // When
-        int x = 0, y = 0;
-        mineSweeper.update(x, y);
+        mineSweeper.update(0, 0);
 
         // Then
-        assertThat(mineSweeper.board[x][y]).isEqualTo('X');
+        assertThat(mineSweeper.board[0][0]).isEqualTo('X');
+    }
+
+    @Test
+    public void change_an_empty_square_to_the_number_of_adjacent_mines_when_it_is_revealed() {
+        // Given
+        char[][] board = {
+                {'M', 'E', 'E'},
+                {'E', 'E', 'M'},
+                {'E', 'M', 'E'}
+        };
+        MineSweeper mineSweeper = new MineSweeper(board);
+
+        // When
+        mineSweeper.update(0, 1);
+        mineSweeper.update(0, 2);
+        mineSweeper.update(1, 0);
+        mineSweeper.update(1, 1);
+        mineSweeper.update(2, 0);
+        mineSweeper.update(2, 2);
+
+        // Then
+        assertThat(mineSweeper.board[0][1]).isEqualTo('2');
+        assertThat(mineSweeper.board[0][2]).isEqualTo('1');
+        assertThat(mineSweeper.board[1][0]).isEqualTo('2');
+        assertThat(mineSweeper.board[1][1]).isEqualTo('3');
+        assertThat(mineSweeper.board[2][0]).isEqualTo('1');
+        assertThat(mineSweeper.board[2][2]).isEqualTo('2');
     }
 }
